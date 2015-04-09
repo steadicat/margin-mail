3f6cf1bfc8656044ef70e09a80f0d4594bd7d58b
Stefano J. Attardi <stefano@storehouse.co>
2015-04-09 08:45:24 -0700

    Clean up render code a bit

--- Margin Mail/Component.swift
+++ Margin Mail/Component.swift
@@ -21,10 +21,7 @@ class Component: NSObject {
         self.children = children
         super.init()
     }
-    
-    var lastRender: Component?
-    var lastView: NSView?
-    
+
     var dirty = false
     
     func needsRender() {
@@ -34,19 +31,7 @@ class Component: NSObject {
         self.dirty = true
         dispatch_async(dispatch_get_main_queue(), { () -> Void in
             self.dirty = false
-            var newRender = self.render()
-            var newView: NSView?
-            if self.lastRender != nil && newRender.className == self.lastRender!.className {
-                newView = newRender.renderToView(self.lastView, lastRender: self.lastRender)
-            } else {
-                newView = newRender.renderToView(nil, lastRender: nil)
-            }
-            self.lastRender = newRender
-            newRender.lastView = newView
-            if (newView != self.lastView) {
-                self.lastView = newView
-                self.delegate?.componentRendered(newView!)
-            }
+            self.renderSelf(nil)
         })
     }
     
@@ -58,26 +43,49 @@ class Component: NSObject {
         assert(false, "Only view components can render to view")
     }
     
+    
+    var lastRender: Component?
+    var lastView: NSView?
+    
+    func renderSelf(var lastRender: Component?) -> NSView {
+        if self.lastRender != nil {
+            lastRender = self.lastRender
+        }
+        let lastView = lastRender?.lastView
+        
+        var newRender = self.render()
+        var newView: NSView?
+        if newRender.className == lastRender?.className {
+            newView = newRender.renderToView(lastView, lastRender: lastRender)
+        } else {
+            newView = newRender.renderToView(nil, lastRender: nil)
+        }
+        self.lastRender = newRender
+        newRender.lastView = newView
+        if (newView != lastView) {
+            self.lastView = newView
+            self.delegate?.componentRendered(newView!)
+        }
+        return newView!
+    }
+    
     func renderChildren(view: NSView, children: [Component], lastChildren: [Component]) {
         if children.count + lastChildren.count == 0 {
             return
         }
         
         var viewsToRemove: [NSView] = []
+        
         for i in 0...max(children.count - 1, lastChildren.count - 1) {
             if i >= children.count && lastChildren[i].lastView != nil {
                 viewsToRemove.append(lastChildren[i].lastView!)
             } else if i >= lastChildren.count {
-                var render = children[i].render()
-                children[i].lastRender = render;
-                render.lastView = render.renderToView(nil, lastRender: nil)
-                view.addSubview(render.lastView!)
+                view.addSubview(children[i].renderSelf(nil))
             } else {
-                var render = children[i].render()
-                children[i].lastRender = render
-                render.lastView = render.renderToView(lastChildren[i].lastRender!.lastView, lastRender: lastChildren[i].lastRender)
+                children[i].renderSelf(lastChildren[i].lastRender)
             }
         }
+        
         for view in viewsToRemove {
             view.removeFromSuperview()
         }
