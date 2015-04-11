032639ea00f379eb5d751827c7839c6eefae6c74
Stefano J. Attardi <stefano@storehouse.co>
2015-04-11 14:53:49 -0700

    Change copyright notice

--- Margin Mail/AppDelegate.swift
+++ Margin Mail/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  Margin Mail
 //
 //  Created by Stefano J. Attardi on 4/5/15.
-//  Copyright (c) 2015 Stefano J. Attardi. All rights reserved.
+//  Copyright (c) 2015 Margin Labs. All rights reserved.
 //
 
 import Cocoa
