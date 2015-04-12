d53f4e908202cf66f81c4c67e93d09f1ba6610fa
Artem Nezvigin <artem@artnez.com>
2015-04-11 20:55:49 -0700

    Add MailCore2 dependency

---
+++ BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  Margin Mail
+//
+//  Created by Artem Nezvigin on 4/11/15.
+//  Copyright (c) 2015 Stefano J. Attardi. All rights reserved.
+//
+
+#ifndef Margin_Mail_Margin_Mail_Bridging_Header_h
+#define Margin_Mail_Margin_Mail_Bridging_Header_h
+
+#import <MailCore/MailCore.h>
+
+#endif
