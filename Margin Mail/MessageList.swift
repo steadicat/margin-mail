3ce36d4a61c52dbdb8fd92498a7928a6c8df6b07
Stefano J. Attardi <stefano@storehouse.co>
2015-04-11 23:43:18 -0700

    Clean up MessageList cruft

--- Margin Mail/MessageList.swift
+++ Margin Mail/MessageList.swift
@@ -10,44 +10,7 @@ import Cocoa
 
 class MessageList: View {
     
-    var color: NSColor?
-    
-    private var text: TextField
-    private var loadingText: TextField
-    
-    var isLoading: Bool = true {
-        didSet {
-            self.needsDisplay = true
-        }
-    }
-    
-    init(frame: CGRect, color: NSColor? = nil) {
-        self.color = color
-        text = TextField(frame: CGRectZero)
-        loadingText = TextField(frame: CGRectZero)
-        loadingText.text = "Loading..."
-        
-        super.init(frame: frame)
-        
-        self.addSubview(text)
-        self.addSubview(loadingText)
-        
-        weak var weakSelf = self
-        
-        var time = dispatch_time(DISPATCH_TIME_NOW, Int64(8 * Double(NSEC_PER_SEC)))
-        dispatch_after(time, dispatch_get_main_queue(), { weakSelf?.isLoading = false })
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewWillDraw() {
-        backgroundColor = self.color
-        text.frame = CGRect(center: self.bounds.center, size: Size(80, 20)).offset(0, -20)
-        loadingText.frame = CGRect(center: self.bounds.center, size: Size(80, 20)).offset(0, 20)
-        loadingText.alphaValue = self.isLoading ? 1 : 0
-        
         super.viewWillDraw()
     }
 
