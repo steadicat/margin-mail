9b22cef51c1dede0573485e1ce407e2bab1e0943
Artem Nezvigin <artem@artnez.com>
2015-04-17 12:18:16 -0700

    Fix build schemes and other things

--- MarginMailTests/MarginMailTests.swift
+++ MarginMailTests/MarginMailTests.swift
@@ -2,7 +2,7 @@
 //  MarginMailTests.swift
 //  MarginMailTests
 //
-//  Created by Artem Nezvigin on 4/16/15.
+//  Created by Artem Nezvigin on 4/17/15.
 //  Copyright (c) 2015 Margin Labs. All rights reserved.
 //
 
