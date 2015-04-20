c027dd0fedee88d9e05944103c7ac678609016b1
Artem Nezvigin <artem@artnez.com>
2015-04-20 16:47:58 -0700

    Add event system and dispatcher

---
+++ MarginMail/Dispatcher.swift
@@ -0,0 +1,14 @@
+//
+//  Dispatcher.swift
+//  MarginMail
+//
+//  Created by Artem Nezvigin on 4/20/15.
+//  Copyright (c) 2015 Margin Labs. All rights reserved.
+//
+
+class Dispatcher {
+
+    static let MessageArrived = Event<Any?>()
+    static let MessageCreated = Event<Any?>()
+
+}
